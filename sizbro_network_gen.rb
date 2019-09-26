8a9920f62145b5d4b94585cb1e389e3f62f3092a
Warut Surapat <s.warut@gmail.com>
2019-09-26 15:40:28 +0900

    Add text generation.

--- sizbro_network_gen.rb
+++ sizbro_network_gen.rb
@@ -1,11 +1,38 @@
 require 'optparse'
+require 'victor'
+require 'json'
 
-options = {}
+options = {format: 'text'}
 
 OptionParser.new do |parser|
-  parser.on("--output", "output file name.") do |p|
+  parser.on('-o', '--output out', 'output file name.') do |p|
     options[:output] = p
+  end
+  parser.on('-f', '--format format', 'format of the output') do |p|
+    options[:format] = p
   end
 end.parse!
 
-puts "options = #{options}"
+def member_list
+  source = File.read('list.json')
+  JSON.parse(source)
+end
+
+def generate_text(filename)
+  f = File.open(filename, 'w')
+  output = ''
+  member_list.each do |scholar_year|
+
+    output << "#{scholar_year["year"]}th Generation:\n"
+    scholar_year["members"].each do |member|
+      output << "\t- #{member["name"]}(#{member["nickname"]}), #{member["occupation"]}\n"
+    end
+  end
+  f.write(output)
+  f.close
+end
+
+
+send("generate_#{options[:format]}", options[:output])
+
+
